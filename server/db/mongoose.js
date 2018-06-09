eec9b26d53f16eb3e972f2f104bd8e2538c80c8d
sajicode <sajioloye@gmail.com>
2018-06-09 16:38:06 +0100

    set up separate test and devlopment env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,6 @@ var mongoose = require('mongoose');
 // use promises on mongoose
 mongoose.Promise = global.Promise;
 // connect mongoose to db
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
