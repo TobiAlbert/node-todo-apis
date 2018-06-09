3a62096c5bd9bb96f8cffd8c8429d3b896fcb11d
sajicode <sajioloye@gmail.com>
2018-06-09 15:43:12 +0100

    test PATCH /todo/:id route

---
+++ playground/mongoose-update.js
@@ -0,0 +1,6 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('../server/db/mongoose'),
+      {Todo}   = require('../server/models/todo'),
+      {User}   = require('../server/models/user');
+
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 123
 }];
 
 beforeEach((done) => {
@@ -132,6 +134,47 @@ describe("DELETE /todos/:id", () => {
     request(app)
       .delete('/todos/123xyz')
       .expect(404)
+      .end(done);
+  });
+});
+
+describe("PATCH /todos/:id", () => {
+  it("should update a todo", (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text: text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(true)
+        expect(res.body.todo.completedAt).toBeA("number")
+      })
+      .end(done);
+  });
+
+  it("should clear completedAt when todo is not completed", (done) => {
+    var hexId = todos[1]._id.toHexString(),
+        text = 'Trying out the second todo',
+        completed = false;
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text,
+        completed
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(false)
+        expect(res.body.todo.completedAt).toNotExist()
+      })
       .end(done);
   });
 });
